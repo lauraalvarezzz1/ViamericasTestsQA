86b2fe9014c1bcf20ba6046201d8a5d4719c248c
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-01-19 15:36:17 -0500

    All countries for Send Money section.

--- src/com/selenium/viamericas/pages/SendMoney.java
+++ src/com/selenium/viamericas/pages/SendMoney.java
@@ -83,21 +83,60 @@ public class SendMoney {
 		Assert.assertEquals("https://www.govianex.com/#/sendmoney/destination", driver.getCurrentUrl());
 	}
 
-	// Each one
-	public void LoginLabel() {
+	// Send Money complete to Recipient Information form with Bank Account
+	public void CompleteSendMoneyBankAcount() {
+
 		driver.findElement(LoginLabel).click();
-	}
-
-	public void UsernameInput() {
 		driver.findElement(UsernameInput).sendKeys("laura.alvarez@talosdigital.com");
-	}
-
-	public void PasswordInput() {
 		driver.findElement(PasswordInput).sendKeys("Laura123");
+		driver.findElement(SignInButton).click();
+		driver.findElement(GoToSendMoney).click();
+		driver.findElement(CountryName).sendKeys("COLOMBIA");
+		driver.findElement(SelectAnyOption).click();
+		driver.findElement(SendField).sendKeys("5");
+		driver.findElement(BankDepositButton).click();
+		driver.findElement(ChooseaBankField).sendKeys("BANCOLOMBIA");
+		driver.findElement(SelectAnyOption).click();
+		driver.findElement(ContinueButton).click();
+		Assert.assertEquals("https://www.govianex.com/#/sendmoney/recipient", driver.getCurrentUrl());
 	}
 
 	public void countryName() {
+		driver.findElement(CountryName).sendKeys("ARGENTINA"); // <-----------ARGENTINA---------->
+		driver.findElement(CountryName).sendKeys("AUSTRALIA"); // <-----------AUSTRALIA---------->
+		driver.findElement(CountryName).sendKeys("BANGLADESH"); // <-----------BANGLADESH---------->
+		driver.findElement(CountryName).sendKeys("BOLIVIA"); // <-----------BOLIVIA---------->
+		driver.findElement(CountryName).sendKeys("BRAZIL"); // <-----------BRAZIL---------->
+		driver.findElement(CountryName).sendKeys("CANADA"); // <-----------CANADA---------->
+		driver.findElement(CountryName).sendKeys("CHILE"); // <-----------CHILE---------->
 		driver.findElement(CountryName).sendKeys("COLOMBIA"); // <-----------COLOMBIA---------->
+		driver.findElement(CountryName).sendKeys("COSTA RICA"); // <-----------COSTA RICA---------->
+		driver.findElement(CountryName).sendKeys("DOMINICAN REPUBLIC"); // <-----------DOMINICAN REPUBLIC---------->
+		driver.findElement(CountryName).sendKeys("ECUADOR"); // <-----------ECUADOR---------->
+		driver.findElement(CountryName).sendKeys("EL SALVADOR"); // <-----------EL SALVADOR---------->
+		driver.findElement(CountryName).sendKeys("GUATEMALA"); // <-----------GUATEMALA---------->
+		driver.findElement(CountryName).sendKeys("GUYANA"); // <-----------GUYANA---------->
+		driver.findElement(CountryName).sendKeys("HAITI"); // <-----------HAITI---------->
+		driver.findElement(CountryName).sendKeys("HONDURAS"); // <-----------HONDURAS---------->
+		driver.findElement(CountryName).sendKeys("HONG KONG"); // <-----------HONG KONG---------->
+		driver.findElement(CountryName).sendKeys("INDIA"); // <-----------INDIA---------->
+		driver.findElement(CountryName).sendKeys("INDONESIA"); // <-----------INDONESIA---------->
+		driver.findElement(CountryName).sendKeys("KOREA, SOUTH"); // <-----------KOREA, SOUTH---------->
+		driver.findElement(CountryName).sendKeys("MALAYSIA"); // <-----------MALAYSIA---------->
+		driver.findElement(CountryName).sendKeys("MEXICO"); // <-----------MEXICO---------->
+		driver.findElement(CountryName).sendKeys("NEPAL"); // <-----------NEPAL---------->
+		driver.findElement(CountryName).sendKeys("NEW ZEALAND"); // <-----------NEW ZEALAND---------->
+		driver.findElement(CountryName).sendKeys("NICARAGUA"); // <-----------NICARAGUA---------->
+		driver.findElement(CountryName).sendKeys("PERU"); // <-----------PERU---------->
+		driver.findElement(CountryName).sendKeys("PHILIPPINES"); // <-----------PHILIPPINES---------->
+		driver.findElement(CountryName).sendKeys("SPAIN"); // <-----------SPAIN---------->
+		driver.findElement(CountryName).sendKeys("SWITZERLAND"); // <-----------SWITZERLAND---------->
+		driver.findElement(CountryName).sendKeys("TRINIDAD AND TOBAGO"); // <-----------TRINIDAD AND TOBAGO---------->
+		driver.findElement(CountryName).sendKeys("UNITED KINGDOM"); // <-----------UNITED KINGDOM---------->
+		driver.findElement(CountryName).sendKeys("UNITED STATES"); // <-----------UNITED STATES---------->
+		driver.findElement(CountryName).sendKeys("URUGUAY"); // <-----------URUGUAY---------->
+		driver.findElement(CountryName).sendKeys("VIETNAM"); // <-----------VIETNAM---------->
+		
 	}
 
 }
