bec59374e263675541a5104aa5852ba1476316f3
Cristhian Preciado <cpreciado@objectwave.com>
2017-01-31 15:10:35 -0500

    Adding Destination page object and tests

--- src/com/selenium/viamericas/tests/SendMoneyFlow.java
+++ src/com/selenium/viamericas/tests/SendMoneyFlow.java
@@ -1,5 +1,35 @@
 package com.selenium.viamericas.tests;
 
+import com.selenium.viamericas.pages.*;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.selenium.viamericas.utility.Start;
+
 public class SendMoneyFlow {
 
+        @BeforeClass
+        public void start() throws Exception {
+            Start.initiate("dev");
+            HomePage.Goto("login");
+            LoginPage.login("cristhian.preciado@talosdigital.com", "Test4echo");
+            HomePage.Goto("sendmoney");
+        }
+
+        @AfterClass
+        public void finish() {
+            Start.driver.quit();
+        }
+
+        @Test
+        public void FillDestinationInformation() throws Exception {
+            Send_DestinationPage.selectCountry();
+            Send_DestinationPage.selectamounttoSend();
+            Send_DestinationPage.howMoneyRecieved("BankDeposit");
+            Send_DestinationPage.chooseBank();
+            Assert.assertNotNull(Send_DestinationPage.exchangerate);
+            Send_DestinationPage.goandcontinue();
+            Assert.assertTrue(Start.driver.getCurrentUrl().contains("recipient"));
+        }
 }
