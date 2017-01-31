c811ea8c65792e2078144eaf6797b4ed042b6283
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-01-31 10:53:59 -0500

    Sign Up tests

--- src/com/selenium/viamericas/pages/SignUpPage.java
+++ src/com/selenium/viamericas/pages/SignUpPage.java
@@ -1,6 +1,7 @@
 package com.selenium.viamericas.pages;
 
 import org.openqa.selenium.By;
+import com.selenium.viamericas.utility.Start;
 public class SignUpPage {
 
     //<---------OBLIGATORY--------->
@@ -10,9 +11,9 @@ public class SignUpPage {
     public static By mobilephonenumber = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[1]/div[5]/input");
     public static By addressline1 = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[2]/div[1]/input");
     public static By country = By.xpath(".//*[@placeholder='Country']");
-    public static By state = By.xpath(".//*[@placeholder='State']");
+    public static By stateDropdown = By.xpath(".//*[@placeholder='State']");
     public static By zipcode = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[2]/div[4]/div/div[2]/input");
-    public static By city = By.xpath(".//*[@placeholder='City']");
+    public static By cityDropdown = By.xpath(".//*[@placeholder='City']");
     public static By emailaddress = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[2]/div[8]/input");
     public static By acceptTCPP = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[2]/div[9]/div[1]/ins");
     public static By createbutton = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[3]/button");
@@ -28,4 +29,118 @@ public class SignUpPage {
     public static By year = By.xpath(".//*[@placeholder='Year']");
 
     public static By selectoption = By.xpath(".//*[@id='dropdown-list']");
+
+
+    public static void gotosignupform() throws Exception {
+        Start.driver.findElement(signupbutton).click();
+        Thread.sleep(2000);
+    }
+
+    public static void addfirstname(String first) throws Exception {
+        Thread.sleep(1500);
+        Start.driver.findElement(firstname).clear();
+        Start.driver.findElement(firstname).sendKeys(first);
+    }
+
+    public static void addlastname(String last) throws Exception {
+        Thread.sleep(1500);
+        Start.driver.findElement(lastname).clear();
+        Start.driver.findElement(lastname).sendKeys(last);
+    }
+
+    public static void addmobilephonenumber(String mobile) throws Exception {
+        Thread.sleep(1500);
+        Start.driver.findElement(mobilephonenumber).clear();
+        Start.driver.findElement(mobilephonenumber).sendKeys(mobile);
+    }
+
+    public static void addadressline1(String address) throws Exception {
+        Thread.sleep(1500);
+        Start.driver.findElement(addressline1).clear();
+        Start.driver.findElement(addressline1).sendKeys(address);
+    }
+
+    public static void addzipcode(String zipcod) throws Exception {
+        Thread.sleep(1500);
+        Start.driver.findElement(zipcode).clear();
+        Start.driver.findElement(zipcode).sendKeys(zipcod);
+    }
+
+    public static void addemailaddress(String email) throws Exception {
+        Thread.sleep(1500);
+        Start.driver.findElement(emailaddress).clear();
+        Start.driver.findElement(emailaddress).sendKeys(email);
+    }
+
+    public static void selectcity(String city) {
+        Start.driver.findElement(cityDropdown).clear();
+        Start.driver.findElement(cityDropdown).sendKeys(city);
+        Start.driver.findElement(selectoption).click();
+    }
+
+    public static void selectstate(String state) {
+        Start.driver.findElement(stateDropdown).clear();
+        Start.driver.findElement(stateDropdown).sendKeys(state);
+        Start.driver.findElement(selectoption).click();
+    }
+
+    public static void selectcountry(String countr) {
+        Start.driver.findElement(country).clear();
+        Start.driver.findElement(country).sendKeys(countr);
+        Start.driver.findElement(selectoption).click();
+    }
+
+    public static void acceptTCPP()throws Exception{
+        Start.driver.findElement(acceptTCPP).click();
+        Thread.sleep(2000);
+    }
+
+    public static void createbutton() throws Exception{
+        Start.driver.findElement(createbutton).click();
+        Thread.sleep(2000);
+    }
+
+    //<----------------Optional----------------->
+
+    public static void addmiddlename(String middle) throws Exception {
+        Thread.sleep(1500);
+        Start.driver.findElement(middlename).clear();
+        Start.driver.findElement(middlename).sendKeys(middle);
+    }
+
+    public static void addsecondlastname(String second) throws Exception {
+        Thread.sleep(1500);
+        Start.driver.findElement(secondlastname).clear();
+        Start.driver.findElement(secondlastname).sendKeys(second);
+    }
+
+    public static void addaddressline2(String address2) throws Exception {
+        Thread.sleep(1500);
+        Start.driver.findElement(addressline2).clear();
+        Start.driver.findElement(addressline2).sendKeys(address2);
+    }
+
+    public static void addcountryofbirth(String countryofb) throws Exception {
+        Start.driver.findElement(countryofbirth).clear();
+        Start.driver.findElement(countryofbirth).sendKeys(countryofb);
+        Start.driver.findElement(selectoption).click();
+    }
+
+    public static void addmonth(String monthdropdown) throws Exception {
+        Start.driver.findElement(month).clear();
+        Start.driver.findElement(month).sendKeys(monthdropdown);
+        Start.driver.findElement(selectoption).click();
+    }
+
+    public static void addday(String daydropdown) throws Exception {
+        Start.driver.findElement(day).clear();
+        Start.driver.findElement(day).sendKeys(daydropdown);
+        Start.driver.findElement(selectoption).click();
+    }
+
+    public static void addyear(String yeardropdown) throws Exception {
+        Start.driver.findElement(year).clear();
+        Start.driver.findElement(year).sendKeys(yeardropdown);
+        Start.driver.findElement(selectoption).click();
+    }
 }
--- src/com/selenium/viamericas/tests/SignUp.java
+++ src/com/selenium/viamericas/tests/SignUp.java
@@ -1,7 +1,62 @@
 package com.selenium.viamericas.tests;
 
+import com.selenium.viamericas.pages.SignUpPage;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.selenium.viamericas.utility.Start;
+
 /**
  * Created by lauraalvarez on 30/01/17.
  */
 public class SignUp {
+
+
+    @BeforeClass
+    public void start() throws Exception {
+        Start.initiate("dev");
+    }
+
+    @AfterClass
+    public void finish() {
+        Start.driver.quit();
+    }
+
+    @Test(enabled = true, priority = 0)
+    public void completestringfields() throws Exception {
+        SignUpPage.addfirstname("LAURA");
+        SignUpPage.addlastname("ALVAREZ");
+        SignUpPage.addmobilephonenumber("3117224425");
+        SignUpPage.addadressline1("Calle 10");
+        SignUpPage.addzipcode("90001");
+        SignUpPage.addemailaddress("testviamericas@gmail.com");
+        Assert.assertNull("");
+
+
+    }
+
+    @Test(enabled = true, priority = 1)
+    public void validatecharacteres() throws Exception {
+
+    }
+
+    @Test(enabled = true, priority = 2)
+    public void validatenumbers() throws Exception {
+
+    }
+
+    @Test(enabled = true, priority = 3)
+    public void optionalfieldsignup() throws Exception {
+
+    }
+
+    @Test(enabled = true, priority = 4)
+    public void completedropdowns() throws Exception {
+        SignUpPage.selectcountry("COLOMBIA");
+
+    }
+
 }
+
+
