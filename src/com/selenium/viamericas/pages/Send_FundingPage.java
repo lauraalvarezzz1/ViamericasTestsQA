7f7d020949e4a51a98a68faab48b023c69d35f46
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-01-31 22:00:14 -0500

    Funding Page

--- src/com/selenium/viamericas/pages/Send_FundingPage.java
+++ src/com/selenium/viamericas/pages/Send_FundingPage.java
@@ -1,7 +1,107 @@
 package com.selenium.viamericas.pages;
 
-/**
- * Created by talosdigital on 1/28/17.
- */
+import com.selenium.viamericas.utility.Start;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
 public class Send_FundingPage {
+
+    public static By bankaccountbutton = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[1]/div[1]/div/div");
+    public static By creditaccountbutton = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[1]/div[2]/div/div");
+    public static By continueButton = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[2]/button[2]");
+    public static By backButton = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[2]/button[1]");
+
+    //<--------BANK ACCOUNT BUTTON--------->
+    public static By accountholdername = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[2]/input");
+    public static By accountnickname = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[3]/input");
+    public static By routingname = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[4]/input");
+    public static By accountnumber = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[5]/input");
+    public static By accounttype = By.xpath(".//*[@placeholder='Account Type']");
+
+    //<--------CREDIT/DEBIT BUTTON----------->
+    public static By cardholdername = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[3]/input");
+    public static By cardnickname = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[4]/input");
+    public static By cardnumber = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[5]/input");
+    public static By monthdropdown = By.xpath(".//*[@placeholder='Month]");
+    public static By yeardropdown = By.xpath(".//*[@placeholder='Year']");
+    public static By CVVcode = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div/form/div[1]/div[2]/div[7]/input");
+
+    public static void selectaccount(String selectaccountoption) throws Exception {
+        Thread.sleep(3000);
+        if (selectaccountoption.equalsIgnoreCase("bankaccount")) {
+            Start.driver.findElement(bankaccountbutton).click();
+        } else if (selectaccountoption.equalsIgnoreCase("creditdebitbutton")) {
+            Start.driver.findElement(creditaccountbutton).click();
+        }
+    }
+
+    //Bank Account
+    public static void addbankholdername() throws Exception {
+        Thread.sleep(3000);
+        Start.driver.findElement(accountholdername).sendKeys("LAURA ALVAREZ");
+    }
+
+    public static void addbanknickname() throws Exception {
+        Thread.sleep(3000);
+        Start.driver.findElement(accountnickname).sendKeys("LAURA ALVAREZ");
+    }
+
+    public static void addroutingnumber() throws Exception {
+        Thread.sleep(3000);
+        Start.driver.findElement(routingname).sendKeys("0000011000000");
+    }
+
+    public static void addnumber() throws Exception {
+        Thread.sleep(3000);
+        Start.driver.findElement(accountnumber).sendKeys("11010101110");
+    }
+
+    public static void selecttype() throws Exception {
+        Start.driver.findElement(accounttype).sendKeys("SAVING");
+        Start.driver.findElement(accounttype).sendKeys(Keys.DOWN);
+        Start.driver.findElement(accounttype).sendKeys(Keys.ENTER);
+        Thread.sleep(1000);
+    }
+
+    //Credit/Debit
+    public static void addcardholdername() throws Exception {
+        Thread.sleep(3000);
+        Start.driver.findElement(cardholdername).sendKeys("JAYDEN ADRIAN");
+    }
+
+    public static void addcardnickname() throws Exception {
+        Thread.sleep(3000);
+        Start.driver.findElement(cardnickname).sendKeys("JAYDEN ADRIAN");
+    }
+
+    public static void addcardnumber() throws Exception {
+        Thread.sleep(3000);
+        Start.driver.findElement(cardnumber).sendKeys("4716402136742264");
+    }
+
+    public static void selectmonth() throws Exception {
+        Start.driver.findElement(monthdropdown).sendKeys("01");
+        Start.driver.findElement(monthdropdown).sendKeys(Keys.DOWN);
+        Start.driver.findElement(monthdropdown).sendKeys(Keys.ENTER);
+        Thread.sleep(1000);
+    }
+
+    public static void selectyear() throws Exception {
+        Start.driver.findElement(yeardropdown).sendKeys("2018");
+        Start.driver.findElement(yeardropdown).sendKeys(Keys.DOWN);
+        Start.driver.findElement(yeardropdown).sendKeys(Keys.ENTER);
+        Thread.sleep(1000);
+    }
+
+    public static void addcvvcode() throws Exception {
+        Thread.sleep(3000);
+        Start.driver.findElement(CVVcode).sendKeys("505");
+    }
+
+    public static void continuebutton() throws Exception {
+        Thread.sleep(4000);
+        Start.driver.findElement(continueButton).click();
+
+    }
 }
+
