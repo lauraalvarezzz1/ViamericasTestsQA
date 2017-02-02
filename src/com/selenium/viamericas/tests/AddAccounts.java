0f7f6a26697ef8c6b5c708c42f632157a333ee77
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-02-02 16:35:27 -0500

    Add accounts test

--- src/com/selenium/viamericas/tests/AddAccounts.java
+++ src/com/selenium/viamericas/tests/AddAccounts.java
@@ -1,5 +1,60 @@
 package com.selenium.viamericas.tests;
 
+import com.selenium.viamericas.pages.HomePage;
+import com.selenium.viamericas.pages.LoginPage;
+import com.selenium.viamericas.pages.MyAccount;
+import com.selenium.viamericas.subpages.Accounts;
+import com.selenium.viamericas.subpages.TCPPHistoryPage;
+import com.selenium.viamericas.utility.Start;
+import com.sun.jna.platform.win32.Advapi32Util;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class AddAccounts {
 
+    @BeforeClass
+    public void start() throws Exception {
+        Start.initiate("dev");
+        HomePage.Goto("login");
+        LoginPage.login("laura.alvarez@talosdigital.com", "Laura123");
+        HomePage.Goto("accounts");
+    }
+
+    @AfterClass
+    public void finish() {
+        Start.driver.quit();
+    }
+
+    @Test(enabled = true, priority = 0)
+    public void gotoMyaccount() throws Exception {
+        MyAccount.gotoaccountlabel();
+        MyAccount.gotocardandbankaccounts();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("accounts"));
+    }
+
+    @Test(enabled = true, priority = 2)
+    public void Createcardaccount() throws Exception {
+        Accounts.clickoncardAccount();
+        Accounts.addthecardholdername();
+        Accounts.addcardnumber();
+        Accounts.selectmonth();
+        Accounts.selectyear();
+        Accounts.addcvvcode();
+        Accounts.clickoncreateaccount();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("accounts"));
+    }
+
+    @Test(enabled = true, priority = 1)
+    public void bankaccount() throws Exception {
+        Accounts.clickoncreatebutton();
+        Accounts.addbankholdername();
+        Accounts.addbanknickname();
+        Accounts.addroutingnumber();
+        Accounts.addaccountnumber();
+        Accounts.selecttype();
+        Accounts.clickoncreatebutton();
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("accounts"));
+    }
 }
