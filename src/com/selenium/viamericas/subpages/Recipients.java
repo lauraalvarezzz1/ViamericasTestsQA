5e719c40ef3f688db3bcacdf2a0cbdabe1cdf793
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-02-07 14:59:11 -0500

    xpath for important variables in Recipients.

--- src/com/selenium/viamericas/subpages/Recipients.java
+++ src/com/selenium/viamericas/subpages/Recipients.java
@@ -7,7 +7,7 @@ public class Recipients {
 
 	WebDriver driver;
 
-	// It's optional because here is created the page: ProfileSettings
+	// It's optional because here is created the page: Recipients
 	public static By LoginLabel = By.xpath("html/body/div[2]/div/div[1]/div[1]/header/div/ul/li[3]/a");
 	public static By UsernameInput = By
 			.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[1]/input");
@@ -18,7 +18,46 @@ public class Recipients {
 	public static By MyAccountLabel = 
 			By.xpath("html/body/div[2]/div/div[1]/div[1]/header/div/ul/li[4]/a");
 	
-	// <-----------PROFILE SETTINGS SECTION------------->
+	// <-----------RECIPIENT------------->
 	public static By RecipientsLabel = By
 			.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div/div[1]/ul/li[2]/a");
+	public static By createButton = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[1]/div/button");
+	public static By firstName = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/form[1]/div[1]/input");
+	public static By middleName = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/form[1]/div[2]/input");
+	public static By lastName = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/form[1]/div[3]/input");
+	public static By secondLastName = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/form[1]/div[4]/input");
+	public static By mobilePhoneNumber = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/form[1]/div[5]/input");
+	public static By emailAddressOptional = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/form[1]/div[6]/input");
+	public static By AddressLine1 = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/form[1]/div[8]/input");
+	public static By AdressLine2 = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/form[1]/div[9]/input");
+	public static By zipCode = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/form[1]/div[11]/div/div[2]/input");
+
+	public static By monthdropdown = By.xpath(".//*[@placeholder='Month']");
+	public static By daydropdown = By.xpath(".//*[@placeholder='Day']");
+	public static By yeardropdown = By.xpath(".//*[@placeholder='Year']");
+
+	public static By countrydropdown = By.xpath(".//*[@placeholder='Country']");
+	public static By statedropdown = By.xpath(".//*[@placeholder='State']");
+	public static By citydropdown = By.xpath(".//*[@placeholder='City']");
+
+	//<--------NEW BANK ACCOUNT----------->
+	public static By currency = By.xpath(".//*[@placeholder='Currency']");
+	public static By chooseabank = By.xpath(".//*[@placeholder='Choose a bank']");
+
+	public static By accountHolderName = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/form[2]/div[2]/div[4]/input");
+	public static By accountNumber = By
+			.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/form[2]/div[2]/div[5]/input");
+	public static By accountType = By.xpath(".//*[@placeholder='Account Type']");
+	public static By createbuttonwithintheform = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[3]/div/div/button");
 }
