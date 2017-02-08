032a70cde65daaa3606cac43d8af3b1f419f4b36
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-02-08 15:23:21 -0500

    Improve the spaces on payerNetwork

--- src/com/selenium/viamericas/pages/PayerNetworkPage.java
+++ src/com/selenium/viamericas/pages/PayerNetworkPage.java
@@ -6,277 +6,240 @@ import org.openqa.selenium.Keys;
 
 public class PayerNetworkPage {
 
-    public static By GotoPayerNetwork = By
-            .xpath("/html/body/div[2]/div/div[2]/footer/div/nav/ul[1]/li[5]/a");
-
-    public static By Country = By
-            .xpath("//*[@id=\"dropdown-input\"]");
+    public static By GotoPayerNetwork = By.xpath("/html/body/div[2]/div/div[2]/footer/div/nav/ul[1]/li[5]/a");
+    public static By Country = By.xpath("//*[@id=\"dropdown-input\"]");
 
     public static void setGotoPayerNetwork() throws Exception {
         Start.driver.findElement(GotoPayerNetwork).click();
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectArgentina() throws Exception {
         Start.driver.findElement(Country).sendKeys("ARGENTINA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectAustralia() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("AUSTRALIA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectBangladesh() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("BANGLADESH");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectBolivia() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("BOLIVIA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectBrazil() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("BRAZIL");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectCanada() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("CANADA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectChile() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("CHILE");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectColombia() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("COLOMBIA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectCostaRica() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("COSTA RICA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectDomininicanRepublic() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("DOMINICAN REPUBLIC");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectEcuador() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("ECUADOR");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectElSalvador() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("EL SALVADOR");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectGuatemala() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("GUATEMALA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectGuyana() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("GUYANA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectHaiti() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("HAITI");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectHonduras() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("HONDURAS");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectHongKong() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("HONG KONG");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectIndia() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("INDIA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectIndonesia() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("INDONESIA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectKoreaSouth() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("KOREA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectMalaysia() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("MALAYSIA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectMexico() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("MEXICO");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectNepal() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("NEPAL");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectNewZealand() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("NEW ZEALAND");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectNicaragua() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("NICARAGUA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectPanama() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("PANAMA");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectPeru() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("PERU");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectPhilippines() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("PHILIPPINES");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectSwitzerland() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("SWITZERLAND");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectUnitedKigdom() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("UNITED KINGDOM");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectUnitedStates() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("UNITED STATES");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectUruguay() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("URUGUAY");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 
     public static void selectVietnam() throws Exception {
         Start.driver.findElement(Country).clear();
         Start.driver.findElement(Country).sendKeys("VIETNAM");
         Start.driver.findElement(Country).sendKeys(Keys.DOWN);
         Start.driver.findElement(Country).sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
-    }
+        Thread.sleep(5000);}
 }
--- src/com/selenium/viamericas/tests/PayerNetwokTest.java
+++ src/com/selenium/viamericas/tests/PayerNetwokTest.java
@@ -15,9 +15,7 @@ import org.testng.annotations.Test;
 public class PayerNetwokTest {
 
     @BeforeClass
-    public void start() throws Exception {
-        Start.initiate("dev");
-    }
+    public void start() throws Exception {Start.initiate("dev");}
 
     @AfterClass
     public void finish() {
@@ -27,198 +25,165 @@ public class PayerNetwokTest {
     @Test(enabled = true, priority = 0)
     public void gotopayernetwork() throws Exception {
         PayerNetworkPage.setGotoPayerNetwork();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 1)
     public void selectoptionforargentina() throws Exception {
         PayerNetworkPage.selectArgentina();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 2)
     public void selectoptionforaustralia() throws Exception {
         PayerNetworkPage.selectAustralia();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 3)
     public void selectoptionforbangladesh() throws Exception {
         PayerNetworkPage.selectBangladesh();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 4)
     public void selectoptionforbrazil() throws Exception {
         PayerNetworkPage.selectBrazil();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 5)
     public void selectoptionforcanada() throws Exception {
         PayerNetworkPage.selectCanada();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 6)
     public void selectoptionforchile() throws Exception {
         PayerNetworkPage.selectChile();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 7)
     public void selectoptionforcolombia() throws Exception {
         PayerNetworkPage.selectColombia();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 8)
     public void selectoptionforcostarica() throws Exception {
         PayerNetworkPage.selectCostaRica();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 9)
     public void selectoptionfordominicanrepublic() throws Exception {
         PayerNetworkPage.selectDomininicanRepublic();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 10)
     public void selectoptionforecuador() throws Exception {
         PayerNetworkPage.selectEcuador();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 11)
     public void selectoptionforelsalvador() throws Exception {
         PayerNetworkPage.selectElSalvador();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 12)
     public void selectoptionforguatemala() throws Exception {
         PayerNetworkPage.selectGuatemala();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 13)
     public void selectoptionforguyana() throws Exception {
         PayerNetworkPage.selectGuyana();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 14)
     public void selectoptionforhaiti() throws Exception {
         PayerNetworkPage.selectHaiti();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 15)
     public void selectoptionforhonduras() throws Exception {
         PayerNetworkPage.selectHonduras();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 16)
     public void selectoptionforhongkong() throws Exception {
         PayerNetworkPage.selectHongKong();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 17)
     public void selectoptionforindia() throws Exception {
         PayerNetworkPage.selectIndia();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 18)
     public void selectoptionforindonesia() throws Exception {
         PayerNetworkPage.selectIndonesia();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 19)
     public void selectoptionforkorea() throws Exception {
         PayerNetworkPage.selectKoreaSouth();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 20)
     public void selectoptionformalaysia() throws Exception {
         PayerNetworkPage.selectMalaysia();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 21)
     public void selectoptionformexico() throws Exception {
         PayerNetworkPage.selectMexico();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 22)
     public void selectoptionfornepal() throws Exception {
         PayerNetworkPage.selectNepal();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 23)
     public void selectoptionfornewzealand() throws Exception {
         PayerNetworkPage.selectNewZealand();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 24)
     public void selectoptionfornicaragua() throws Exception {
         PayerNetworkPage.selectNicaragua();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 25)
     public void selectoptionforpanama() throws Exception {
         PayerNetworkPage.selectPanama();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 26)
     public void selectoptionforperu() throws Exception {
         PayerNetworkPage.selectPeru();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 27)
     public void selectoptionforPhilippines() throws Exception {
         PayerNetworkPage.selectPhilippines();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 28)
     public void selectoptionforSwitzerland() throws Exception {
         PayerNetworkPage.selectSwitzerland();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 29)
     public void selectoptionforUnitedKigdom() throws Exception {
         PayerNetworkPage.selectUnitedKigdom();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 30)
     public void selectoptionforUnitedStates() throws Exception {
         PayerNetworkPage.selectUnitedStates();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 31)
     public void selectoptionforUruguay() throws Exception {
         PayerNetworkPage.selectUruguay();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 
     @Test(enabled = true, priority = 32)
     public void selectoptionforVietnam() throws Exception {
         PayerNetworkPage.selectVietnam();
-        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));
-    }
+        Assert.assertTrue(Start.driver.getCurrentUrl().contains("correspondents"));}
 }
