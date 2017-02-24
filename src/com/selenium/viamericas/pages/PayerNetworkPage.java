92f9b965b8512301064f1ca1f22a0d1264cdfda9
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-02-24 08:30:03 -0500

    Changes in time and another things (add paths and change address)

--- src/com/selenium/viamericas/pages/PayerNetworkPage.java
+++ src/com/selenium/viamericas/pages/PayerNetworkPage.java
@@ -3,6 +3,8 @@ package com.selenium.viamericas.pages;
 import com.selenium.viamericas.utility.Start;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PayerNetworkPage {
 
