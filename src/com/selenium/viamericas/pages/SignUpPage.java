067091f8367454184ac5d43492387580f2ca41d6
Laura Alvarez <laura.alvarez@talosdigital.com>
2017-02-28 12:41:18 -0500

    Changes in Sign up

--- src/com/selenium/viamericas/pages/SignUpPage.java
+++ src/com/selenium/viamericas/pages/SignUpPage.java
@@ -10,28 +10,34 @@ public class SignUpPage {
 
 
     //<---------OBLIGATORY--------->
+    public static By emailaddress = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[1]/input");
+    public static By password = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[2]/input");
+    public static By signupbutton2 = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[3]/div/div[2]/button");
+    public static By cancelbutton = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[3]/div/div[1]/button");
+
     public static By signupbutton = By.xpath("/html/body/div[2]/div/div[1]/div[1]/header/div/ul/li[3]/button[1]");
-    public static By firstname = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[1]/div[1]/input");
-    public static By lastname = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[1]/div[3]/input");
-    public static By mobilephonenumber = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[1]/div[5]/input");
-    public static By addressline1 = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[2]/div[1]/input");
+    public static By firstname = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div/div[1]/div[1]/input");
+    public static By lastname = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div/div[1]/div[2]/input");
+    public static By mobilephonenumber = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div/div[2]/div/input");
+    public static By addressline1 = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div/div[4]/div[1]/input");
     public static By country = By.xpath(".//*[@placeholder='Country']");
     public static By stateDropdown = By.xpath(".//*[@placeholder='State']");
-    public static By zipcode = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[2]/div[4]/div/div[2]/input");
+    public static By zipcode = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div/div[4]/div[3]/div/div[1]/input");
     public static By cityDropdown = By.xpath(".//*[@placeholder='City']");
-    public static By emailaddress = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[2]/div[8]/input");
-    public static By acceptTCPP = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[2]/div[9]/div[1]/ins");
-    public static By createbutton = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[3]/button");
+    public static By acceptTCPP = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div/div[4]/div[7]/div[1]/ins");
+    public static By createaccountbutton = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div/div[4]/div[9]/div/div[2]/button");
+    public static By month = By.xpath(".//*[@placeholder='Month']");
+    public static By day = By.xpath(".//*[@placeholder='Day']");
+    public static By year = By.xpath(".//*[@placeholder='Year']");
+
 
     //<----------OPTIONAL----------->
 
     public static By middlename = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[1]/div[2]/input");
     public static By secondlastname = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[1]/div[4]/input");
-    public static By addressline2 = By.xpath("html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div[2]/div[2]/input");
+    public static By addressline2 = By.xpath("/html/body/div[2]/div/div[1]/div[2]/div/div/div[2]/div/form/div/div[4]/div[2]/input");
     public static By countryofbirth = By.xpath(".//*[@placeholder='Country of Birth']");
-    public static By month = By.xpath(".//*[@placeholder='Month']");
-    public static By day = By.xpath(".//*[@placeholder='Day']");
-    public static By year = By.xpath(".//*[@placeholder='Year']");
+
 
     public static By selectoption = By.xpath(".//*[@id='dropdown-list']");
     //*[@id="dropdown-list"]
@@ -43,6 +49,23 @@ public class SignUpPage {
 
     public static void gotosignupform() throws Exception {
         Start.driver.findElement(signupbutton).click();
+        Thread.sleep(2000);
+    }
+
+    public static void addemailaddress(String email) throws Exception {
+        Thread.sleep(1500);
+        Start.driver.findElement(emailaddress).clear();
+        Start.driver.findElement(emailaddress).sendKeys(email);
+    }
+
+    public static void addpassword(String password1) throws Exception {
+        Thread.sleep(1500);
+        Start.driver.findElement(password).clear();
+        Start.driver.findElement(password).sendKeys(password1);
+    }
+
+    public static void clickonsignupbutton() throws Exception {
+        Start.driver.findElement(signupbutton2).click();
         Thread.sleep(2000);
     }
 
@@ -77,12 +100,6 @@ public class SignUpPage {
         Start.driver.findElement(zipcode).sendKeys(zipcod);
     }
 
-    public static void addemailaddress(String email) throws Exception {
-        Thread.sleep(1500);
-        Start.driver.findElement(emailaddress).clear();
-        Start.driver.findElement(emailaddress).sendKeys(email);
-    }
-
     public static void selectcountry(String countr) throws Exception {
         Start.driver.findElement(country).sendKeys(countr);
         Start.driver.findElement(country).sendKeys(Keys.DOWN);
@@ -111,8 +128,23 @@ public class SignUpPage {
     }
 
     public static void createbutton() throws Exception {
-        Start.driver.findElement(createbutton).click();
+        Start.driver.findElement(createaccountbutton).click();
         Thread.sleep(2000);
+    }
+
+    public static void addmonth(String monthdropdown) throws Exception {
+        Start.driver.findElement(month).clear();
+        Start.driver.findElement(month).sendKeys(monthdropdown);
+    }
+
+    public static void addday(String daydropdown) throws Exception {
+        Start.driver.findElement(day).clear();
+        Start.driver.findElement(day).sendKeys(daydropdown);
+    }
+
+    public static void addyear(String yeardropdown) throws Exception {
+        Start.driver.findElement(year).clear();
+        Start.driver.findElement(year).sendKeys(yeardropdown);
     }
 
     //<----------------Optional----------------->
@@ -138,21 +170,6 @@ public class SignUpPage {
     public static void addcountryofbirth(String countryofb) throws Exception {
         Start.driver.findElement(countryofbirth).clear();
         Start.driver.findElement(countryofbirth).sendKeys(countryofb);
-    }
-
-    public static void addmonth(String monthdropdown) throws Exception {
-        Start.driver.findElement(month).clear();
-        Start.driver.findElement(month).sendKeys(monthdropdown);
-    }
-
-    public static void addday(String daydropdown) throws Exception {
-        Start.driver.findElement(day).clear();
-        Start.driver.findElement(day).sendKeys(daydropdown);
-    }
-
-    public static void addyear(String yeardropdown) throws Exception {
-        Start.driver.findElement(year).clear();
-        Start.driver.findElement(year).sendKeys(yeardropdown);
     }
 
     public void selectanyoption() throws InterruptedException {
